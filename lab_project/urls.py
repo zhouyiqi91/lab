6d327cf43394514aebe1b53e3c77bf592f279657
zhouyiqi91 <root@SGR09.sgr.com>
2018-12-29 15:14:05 +0800

    add templates

--- lab_project/urls.py
+++ lab_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from lab.views import *
@@ -21,5 +21,5 @@ import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',welcome),
+    url(r'^lab/', include('lab.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
